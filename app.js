c96d0ec4c50c4b04c11501b6dc290fccc73b81c4
RedensCoder <andreysidorov04@mail.ru>
2025-09-16 02:32:02 +0300

    🔒Security fixed

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const express = require("express");
 const cors = require("cors");
 const {config} = require("dotenv");
+const fs = require("fs");
+
+const { readFile } = require("./util/storage");
 
 const User = require("./handlers/users");
 
 config();
 
-const app = express();
+const startServer = async () => {
+    const certBuffer = await readFile("ca.crt");
 
-app.use(cors())
-app.use(express.json());
+    const certPath = "ca.crt";
+    fs.writeFileSync(certPath, certBuffer);
 
-app.use("/user", User);
+    const app = express();
 
-const server = app.listen(8080, () => {
-    console.log(`Server started...`);
+    app.use(cors())
+    app.use(express.json());
+
+    app.use("/user", User);
+
+    const server = app.listen(8080, () => {
+        console.log(`Server started...`);
+    });
+}
+
+startServer().catch((err) => {
+    console.error("Ошибка запуска:", err);
+    process.exit(1);
 });
